bb32898f71f6f0037489cc410ad43350b1dc9f46
Sebastian Paz <75841582@certus.edu.pe>
2026-04-16 00:56:29 -0500

    primera carga

---
+++ login.js
@@ -0,0 +1,74 @@
+let users = JSON.parse(localStorage.getItem("users"));
+
+if (!users) {
+    users = [
+        { email: "admin@gmail.com", password: "1234" }
+    ];
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function register() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    let existe = false;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            existe = true;
+            break;
+        }
+    }
+
+    if (existe) {
+        alert("El usuario ya existe");
+        return;
+    }
+
+    users.push({ email, password });
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("Registro exitoso ✅");
+    window.location.href = "login.html";
+}
+
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    let encontrado = false;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email && users[i].password === password) {
+            encontrado = true;
+            break;
+        }
+    }
+
+if (encontrado) {
+
+    document.getElementById("message").innerText = "Welcome 🎉";
+
+    localStorage.setItem("activeUser", email);
+    setTimeout(() => {
+        window.location.href = "home.html";
+    }, 1000);
+
+} else {
+    document.getElementById("message").innerText = "Incorrect data ❌";
+}
+}
+
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
